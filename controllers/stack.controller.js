0ef7b0316c3a15964fd5ac16a3b102171c6574a3
Santa <santiago.aresq@gmail.com>
2021-04-09 10:33:44 -0300

    add validator

--- controllers/stack.controller.js
+++ controllers/stack.controller.js
@@ -1,10 +1,22 @@
 "use strict";
+
+const e = require("express");
+
 function getStack(req, res) {
   res.status(200).json(req.dataProvider.getStack());
 }
 function pushNumber(req, res) {
-  req.dataProvider.pushNumber(req.params.number);
-  res.status(200).json(req.dataProvider.getStack());
+  if (isNaN(req.params.number)) {
+    res
+      .status(400)
+      .json(
+        "Send a correct number value, strings or other data type is not allowed."
+      );
+  } else {
+    // const num = Number(req.params.number);
+    req.dataProvider.pushNumber(req.params.number);
+    res.status(200).json(req.dataProvider.getStack());
+  }
 }
 function popNumber(req, res) {
   req.dataProvider.popNumber();
